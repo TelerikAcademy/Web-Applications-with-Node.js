916655840520d5b7b5084f81e6a22e410ec2b643
Doncho Minkov <DonchoMinkov@gmail.com>
2016-11-22 18:46:08 +0200

    Added eslintrc and eslintignore

--- Live-demos/02. Web-apps-with-Express.js-and-Jade/superheroes-app/data/fraction-data.js
+++ Live-demos/02. Web-apps-with-Express.js-and-Jade/superheroes-app/data/fraction-data.js
@@ -1,12 +1,20 @@
 module.exports = function(models) {
-    let Fraction = models.Fraction;
+    let {
+        Fraction
+    } = models;
 
     return {
         getAllFractions() {
+            return new Promise((resolve, reject) => {
+                Fraction.find((err, fractions) => {
+                    if (err) {
+                        return reject(err);
+                    }
 
+                    return resolve(fractions);
+                });
+            });
         },
-        createFraction(name, superheroes) {
-
-        }
+        createFraction(name, superheroes) {}
     };
 };
